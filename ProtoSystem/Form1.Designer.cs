ffe18d7314f2ce0d460d4f3bdb59f60d9bf81622
Sergyzz <okonkwosergio80@gmail.com>
2024-11-05 17:36:34 +0000

    Added code to the INSERT button for adding assets into databse.

--- ProtoSystem/Form1.Designer.cs
+++ ProtoSystem/Form1.Designer.cs
@@ -162,7 +162,7 @@
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(192, 302);
+            this.dateTimePicker1.Location = new System.Drawing.Point(192, 297);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(182, 27);
             this.dateTimePicker1.TabIndex = 6;
